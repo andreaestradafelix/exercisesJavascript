3ff79b3e1543e015ad9dd05fab09f6fb55e781b4
Andrea Estrada <andreaestradafelix@gmail.com>
2025-12-09 18:59:27 -0700

    Feat: Corregí el input del ejercicio 6

--- src/exercise06.js
+++ src/exercise06.js
@@ -1,5 +1,5 @@
 
-let array = [3,6,12,15,1];
+let array = [3,6,12,5,100,1];
 console.log(`Original: ${array.join(", ")}`);
 for (let i = 0; i < array.length; i++){
     for (let j=0; j<array.length-1; j++){
